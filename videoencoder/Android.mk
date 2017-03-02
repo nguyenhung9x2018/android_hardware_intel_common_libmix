fa4e52e686a1097074b2ac667bcffda507648e42
Alistair Strachan <alistair.strachan@imgtec.com>
2017-03-01 21:23:33 -0800

    Revert "Revert "Remove use of deprecated gralloc APIs.""
    This reverts commit bf7b37e34b1bdebf5e063271a7f1cf6469412e85.
    Bug: 35427231
    Test: netflix, youtube, play movies
    
    Change-Id: I3e148580d51c0a963d13d6ce0ec43f487b56bc4a

--- videoencoder/Android.mk
+++ videoencoder/Android.mk
@@ -34,7 +34,7 @@ endif
 LOCAL_C_INCLUDES := \
     $(TARGET_OUT_HEADERS)/libva \
     $(call include-path-for, frameworks-native) \
-    $(TARGET_OUT_HEADERS)/pvr
+    $(TARGET_OUT_HEADERS)/pvr/hal
 
 ifeq ($(ENABLE_IMG_GRAPHICS),)
 LOCAL_C_INCLUDES += \
